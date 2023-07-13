2c21f27d55eb08846f23951f64aedb28d48bb4e4
ADARSH <adarshkeruvatty@gmail.com>
2023-07-13 10:50:03 +0530

    personal

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -12,17 +12,17 @@ function validateform(){
     }else if(!isNaN(name)){
     alert(" Invalid Name ");
     return false;
-    }else if(text==""||text==null){  
-      alert("Message null??");  
-      return false;  
-      }
+    
 
-      else if(!email.match(mailformat))
+       }   else if(!email.match(mailformat))
       {
         alert("You have entered an invalid email address!");
         document.form1.email.focus();
         return false;
-      }  
+      }  else if(text==""||text==null){  
+        alert("Message null??");  
+        return false;  
+        }
       else
       {
         alert("Form Submitted Successfully");
